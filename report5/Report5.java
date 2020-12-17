bf686b17a4bca1bc88ef433ebe54b8e279dabbac
e195424 <73863165+e195424@users.noreply.github.com>
2020-12-18 08:39:17 +0900

    Update Report5.java
    getMessage()でエラーが出たことを表した.

--- report5/Report5.java
+++ report5/Report5.java
@@ -1,6 +1,19 @@
+package report5;
+
+class getMessage{
+    public getMessage(){
+        System.out.println("error"+" "+"NullPointerException");
+        
+    }
+}
 class Report5{
     public static void main(String[] args){
-        String str=null;
-        System.out.println(str.length());
+            String str=null;
+        try {
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            getMessage gM = new getMessage();
+            e.getMessage();
+        }
     }
 }
