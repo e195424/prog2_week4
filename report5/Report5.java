34f62c8883ed43dd13d9382d053523298a287bdf
e195424 <73863165+e195424@users.noreply.github.com>
2020-12-18 11:11:01 +0900

    Update Report5.java
    strからint型に変換した

--- report5/Report5.java
+++ report5/Report5.java
@@ -1,20 +1,7 @@
-class Message{
-    String message;
-    public String getMessage(){
-        message="errorOFNullPointerException";
-        return this.message;
-        
-    }
-}
 class Report5{
-    public static void main(String[] args){
-            String str=null;
-        try {
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            Message msa = new Message();
-            System.out.println(msa.getMessage());
-            System.out.println(e.getMessage());
-        }
+    public static void main(String[] srgs){
+        String str="百二十三";
+        int value = Integer.parseInt(str);
+        System.out.println(value);
     }
 }
